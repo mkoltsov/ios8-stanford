709e1ee122d549a3211c7685405d9d8bea4f347d
mkoltsov <root@javabean.ru>
2015-03-14 23:46:46 +0100

    reading assignment,part1

--- basics/basics.swift
+++ basics/basics.swift
@@ -21,3 +21,50 @@ for (key, value) in dic {
 }
 
 println(dic["chef"])
+
+func contExtNames(chef:Int=0, pupa:Int=0)->Bool{
+	println ("\(chef) + \(pupa)")
+	return true
+}
+
+contExtNames(chef:1, pupa:2)
+contExtNames()
+
+func backwards(s1:String, s2:String)-> Bool {
+	return s1>s2
+}
+
+var reversed = sorted(names, backwards)
+
+reversed = sorted(names, {(s1:String, s2:String)-> Bool in return s1>s2})
+
+reversed = sorted(names, {s1, s2 in return s1>s2})
+reversed = sorted(names, {s1, s2 in s1>s2})
+reversed = sorted(names, {$0>$1})
+reversed = sorted(names, >)
+reversed = sorted(names){$0>$1}
+
+let digitNames = [0:"Zero", 1:"One", 2:"Two", 3:"Three", 4:"Four",5: "Five", 6: "Six", 7: "Seven", 8: "Eight", 9: "Nine"]
+
+let numbers = [16,58,510]
+
+let strings = numbers.map{
+	(var number) -> String in 
+	var output = ""
+	while number > 0{
+	  output = digitNames[number % 10]! + output
+	  number /= 10
+	}
+	return output
+}
+
+class VideoMode{
+	var chef = "Pupa"
+
+}
+
+let inst = VideoMode()
+
+inst.chef = "Chef"
+
+let vge = VideoMode(chef:"Chef")
